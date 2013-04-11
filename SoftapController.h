4c6981987231f0fd0cf6660533faea8111896286
aftersilence <yuryginpp@gmail.com>
2013-04-11 13:54:52 +0600

    Revert "Clean up softap interface"
    This reverts commit 7e9eb7b48345af69283afa7ca58d3be0a329931b.
    
    Conflicts:
    	SoftapController.cpp
    
    Change-Id: Ieb22f6bca8cde9ca80aff5fa1bf8efc13c29df20

--- SoftapController.h
+++ SoftapController.h
@@ -27,6 +27,8 @@
 #define AP_DRIVER_START_DELAY	800000
 
 class SoftapController {
+    char mBuf[SOFTAP_MAX_BUFFER_SIZE];
+    char mIface[IFNAMSIZ];
     pid_t mPid;
     int mSock;
 
@@ -36,6 +38,8 @@ public:
     SoftapController();
     virtual ~SoftapController();
 
+    int startDriver(char *iface);
+    int stopDriver(char *iface);
     int startSoftap();
     int stopSoftap();
     bool isSoftapStarted();
